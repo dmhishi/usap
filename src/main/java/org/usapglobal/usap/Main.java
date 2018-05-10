5389c0e98130b99432e2b41374621093ff1be3a8
dsmhishi <mhishidestinys@gmail.com>
2018-05-10 01:35:17 -0400

    Complete maven project set up and rest service demo

---
+++ src/main/java/org/usapglobal/usap/Main.java
@@ -0,0 +1,25 @@
+package org.usapglobal.usap;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.sql.*;
+
+@Path("/main")
+public class Main {
+    // This method is called if TEXT_PLAIN is request
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String sayPlainTextHello() {
+        return "Hello Jersey";
+    }
+    // This method is called if HTML is request
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    @Path("/hello")
+    public String sayHtmlHello() {
+        return "<html> " + "<title>" + "Hello Jersey" + "</title>"
+                + "<body><h1>" + "Hello Jersey" + "</body></h1>" + "</html> ";
+    }
+}
